d5ec71a47bcbcf7906271d53d68725f28690d1f9
FEDYA-MASHYNA <126347878+FEDYA-MASHYNA@users.noreply.github.com>
2024-10-24 23:59:11 +0300

    Update index.tsx

--- tech-giants-frontend-master/src/pages/Index/index.tsx
+++ tech-giants-frontend-master/src/pages/Index/index.tsx
@@ -1,10 +1,8 @@
-import {Box, Button, CircularProgress, Container, Stack, Typography, useTheme} from "@mui/material";
-import {useConferenceStore} from "../../stores/conference.store.ts";
-import {useEffect, useState} from "react";
-import {useGlobalStore} from "../../stores/global.store.ts";
+import { Box, Button, CircularProgress, Container, Stack, Typography } from "@mui/material";
+import { useConferenceStore } from "../../stores/conference.store.ts";
+import { useEffect, useState, useRef } from "react";
+import { useGlobalStore } from "../../stores/global.store.ts";
 import { DateTime } from 'luxon';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useSwipeable } from 'react-swipeable';
 
 interface Image {
     src: string;
@@ -17,23 +15,24 @@ interface SwipeableImageProps {
 }
 
 const images = [
-    { src: "https://via.placeholder.com/200/FF0000/FFFFFF?text=Image+1", alt: "Изображение 1" },
-    { src: "https://via.placeholder.com/200/00FF00/FFFFFF?text=Image+2", alt: "Изображение 2" },
-    { src: "https://via.placeholder.com/200/0000FF/FFFFFF?text=Image+3", alt: "Изображение 3" },
+    { src: '../src/components/images/IMG_5580.jpg', alt: "Изображение 1" },
+    { src: '../src/components/images/IMG_5581.jpg', alt: "Изображение 2" },
+    { src: '../src/components/images/IMG_5582.jpg', alt: "Изображение 3" },
+    { src: '../src/components/images/IMG_5584.jpg', alt: "Изображение 4" },
+    { src: '../src/components/images/IMG_5585.jpg', alt: "Изображение 5" },
+    { src: '../src/components/images/IMG_5586.jpg', alt: "Изображение 6" },
+    { src: '../src/components/images/IMG_5587.jpg', alt: "Изображение 7" },
+    { src: '../src/components/images/IMG_5590.jpg', alt: "Изображение 8" },
+    { src: '../src/components/images/IMG_5591.jpg', alt: "Изображение 9" },
+    { src: '../src/components/images/photo_5244767466784156973_y.jpg', alt: "Изображение 10" },
+    { src: '../src/components/images/photo_5244767466784156974_y.jpg', alt: "Изображение 11" },
+    { src: '../src/components/images/photo_5244767466784156975_y.jpg', alt: "Изображение 12" },
 ];
 
 const SwipeableImage: React.FC<SwipeableImageProps> = ({ images }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
-
-    const handlers = useSwipeable({
-        onSwipedLeft: () => {
-            nextImage();
-        },
-        onSwipedRight: () => {
-            prevImage();
-        },
-        trackMouse: true, // Включаем поддержку мыши
-    });
+    const touchStartX = useRef(0);
+    const touchEndX = useRef(0);
 
     const nextImage = () => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
@@ -43,61 +42,83 @@ const SwipeableImage: React.FC<SwipeableImageProps> = ({ images }) => {
         setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
     };
 
+    const onTouchStart = (e: React.TouchEvent) => {
+        touchStartX.current = e.touches[0].clientX; // Запоминаем начальную позицию касания
+    };
+
+    const onTouchMove = (e: React.TouchEvent) => {
+        touchEndX.current = e.touches[0].clientX; // Обновляем конечную позицию касания
+    };
+
+    const onTouchEnd = () => {
+        const diffX = touchStartX.current - touchEndX.current; // Разница между начальной и конечной позициями
+
+        if (diffX > 50) {
+            nextImage(); // Свайп влево
+        } else if (diffX < -50) {
+            prevImage(); // Свайп вправо
+        }
+    };
+
     return (
         <Box
-    {...handlers}
-    sx={{
-        width: { xs: '100%', sm: '400px' },
-        height: { xs: '200px', sm: '400px' },
-        marginLeft: "100px",
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'relative',
-        cursor: 'pointer',
-        userSelect: 'none',
-    }}
->
-    {images.length > 0 ? (
-        <img
-            src={images[currentIndex].src}
-            alt={images[currentIndex].alt}
-            style={{ maxWidth: '100%', maxHeight: '100%', borderRadius: '8px' }}
-        />
-    ) : (
-        <p>Нет изображений для отображения</p>
-    )}
-    <Button
-        onClick={prevImage}
-        sx={{
-            position: 'absolute',
-            left: '10px',
-            top: '50%',
-            transform: 'translateY(-50%)', // Центрируем по вертикали
-            zIndex: 1,
-            margin: '0 -20px',
-            display: { xs: 'none', sm: 'block'  }
-        }}
-    >
-        Назад
-    </Button>
-    <Button
-        onClick={nextImage}
-        sx={{
-            position: 'absolute',
-            right: '10px',
-            top: '50%',
-            transform: 'translateY(-50%)', // Центрируем по вертикали
-            zIndex: 1,
-            margin: '0 -28px',
-            display: { xs: 'none', sm: 'block' },
-        }}
-    >
-        Вперед
-    </Button>
-</Box>
+            onTouchStart={onTouchStart}
+            onTouchMove={onTouchMove}
+            onTouchEnd={onTouchEnd}
+            sx={{
+                width: { xs: '100%', sm: '400px' },
+                height: { xs: '200px', sm: '400px' },
+                marginLeft: "100px",
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                position: 'relative',
+                cursor: 'pointer',
+                userSelect: 'none',
+            }}
+        >
+            {images.length > 0 ? (
+                <img
+                    src={images[currentIndex].src}
+                    alt={images[currentIndex].alt}
+                    style={{ maxWidth: '100%', maxHeight: '100%', borderRadius: '8px' }}
+                />
+            ) : (
+                <p>Нет изображений для отображения</p>
+            )}
+            <Button
+                onClick={prevImage}
+                sx={{
+                    position: 'absolute',
+                    left: '10px',
+                    top: '50%',
+                    transform: 'translateY(-50%)', // Центрируем по вертикали
+                    zIndex: 1,
+                    margin: '0 -73px',
+                    display: { xs: 'none', sm: 'block'  }
+                }}
+            >
+                Назад
+            </Button>
+            <Button
+                onClick={nextImage}
+                sx={{
+                    position: 'absolute',
+                    right: '10px',
+                    top: '50%',
+                    transform: 'translateY(-50%)', // Центрируем по вертикали
+                    zIndex: 1,
+                    margin: '0 -80px',
+                    display: { xs: 'none', sm: 'block' },
+                }}
+            >
+                Вперед
+            </Button>
+        </Box>
     );
 };
+
+
 
 export function Index() {
     const conference = useConferenceStore(state => state.conference);
@@ -184,14 +205,10 @@ export function Index() {
                     <Box sx={{ display: 'flex', flexDirection: { xs: 'column', sm: 'row' }, gap: 2, alignItems: 'center',}}>
     <Box sx={{ flex: 1, }}>
         <Typography variant="body1" sx={{ mt: 2, fontSize: { xs: '0.8rem', sm: '1rem' } }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui. Donec sed odio dui.
+        На кафедре БИТ ИКТИБ проводятся конференции по информационной безопасности.
+        Научно практическая конференция "Информационная безопасность" проводилась с 2003 по 2016 годы. В 2024 году планируется возобновить проведение конференции, присвоив ей имя основателя профессора О.Б. Макаревича
         </Typography>
-        <Typography variant="body1" sx={{ mt: 2, fontSize: { xs: '0.8rem', sm: '1rem' } }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui. Donec sed odio dui.
-        </Typography>
-        <Typography variant="body1" sx={{ mt: 2, fontSize: { xs: '0.8rem', sm: '1rem' } }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui. Donec sed odio dui.
-        </Typography>
+        
     </Box>
 
     <Box sx={{ flex: 1, maxWidth: { xs: '100%', sm: '400px' }, display: 'flex', justifyContent: 'center', marginLeft: "-100px" }}>
